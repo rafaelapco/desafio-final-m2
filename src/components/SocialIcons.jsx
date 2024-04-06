9f180a2709103ce79d5a4f36546e0c54ce9d1be5
Rafaela <rafaela.pacheco01@gmail.com>
2024-04-05 23:44:51 -0300

    ajuste links

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import styled from 'styled-components';
-import { AiFillInstagram, AiFillGithub, AiFillLinkedin  } from "react-icons/ai";
+import React from "react";
+import styled from "styled-components";
+import { AiFillInstagram, AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
 const IconContainer = styled.div`
   display: flex;
   justify-content: start;
   margin-bottom: 20px;
-  margin-left: 80px;  
+  margin-left: 80px;
 `;
 
 const IconLink = styled.a`
   margin: 0 10px;
-  color: #a1a692; 
-  text-decoration: none; 
+  color: #a1a692;
+  text-decoration: none;
   &:hover {
-    color: white; 
+    color: white;
   }
 `;
 
 const SocialIcons = () => {
   return (
     <IconContainer>
-      <IconLink href="https://www.instagram.com/seu_usuario/" target="_blank">
-        <AiFillInstagram size={30} /> 
+      <IconLink href="https://www.instagram.com/rafaela.pco" target="_blank">
+        <AiFillInstagram size={30} />
       </IconLink>
-      <IconLink href="https://www.linkedin.com/in/seu_usuario/" target="_blank">
+      <IconLink href="https://www.linkedin.com/in/rafaela-pco" target="_blank">
         <AiFillLinkedin size={30} />
       </IconLink>
-      <IconLink href="https://github.com/seu_usuario" target="_blank">
+      <IconLink href="https://github.com/rafaelapco" target="_blank">
         <AiFillGithub size={30} />
       </IconLink>
     </IconContainer>
